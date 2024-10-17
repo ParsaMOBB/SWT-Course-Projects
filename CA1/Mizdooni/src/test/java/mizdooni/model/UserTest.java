005d46ca0395e1f4d631dc7563f305e7f48a6405
Amir Parsa Mobed <99682959+ParsaMOBB@users.noreply.github.com>
2024-10-18 01:27:55 +0330

    add test to UserTest

--- CA1/Mizdooni/src/test/java/mizdooni/model/UserTest.java
+++ CA1/Mizdooni/src/test/java/mizdooni/model/UserTest.java
@@ -24,11 +24,47 @@ public class UserTest {
     }
 
     @Test
+    public void check_password_test(){
+        assertTrue(arshia.checkPassword("aa"));
+        assertFalse(arshia.checkPassword("bb"));
+    }
+
+    @Test
     public void reservation_successfully_added_test(){
         arshia.addReservation(reservation1);
         List<Reservation> reservations = arshia.getReservations();
         assertEquals(reservations.size(), 1);
         assertEquals(reservation1.getReservationNumber(), reservations.get(0).getReservationNumber());
+    }
+
+    @Test
+    public void two_reservations_successfully_added_test(){
+        arshia.addReservation(reservation1);
+        Reservation reservation2 = new Reservation(arshia, restaurant1, new Table(2, restaurant1.getId(), 3), LocalDateTime.of(2024, 10, 3, 19, 0, 0));
+        arshia.addReservation(reservation2);
+        List<Reservation> reservations = arshia.getReservations();
+        assertEquals(reservations.size(), 2);
+        assertEquals(reservation2.getReservationNumber(), reservations.get(1).getReservationNumber());
+    }
+
+    @Test 
+    public void check_reserved_no_reservation_test() {
+        assertFalse(arshia.checkReserved(restaurant1));
+        assertEquals(arshia.getReservations().size(), 0);
+    }
+
+    @Test
+    public void check_reserved_test(){
+        arshia.addReservation(reservation1);
+        assertTrue(arshia.checkReserved(restaurant1));
+    }
+
+    @Test
+    public void get_reservation_cancelled_reservation_test(){
+        arshia.addReservation(reservation1);
+        reservation1.cancel();
+        assertNull(arshia.getReservation(0));
+        assertFalse(arshia.checkReserved(restaurant1));
     }
 
 }
